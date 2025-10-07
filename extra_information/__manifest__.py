0aa1c912ac05e6f01182f22c4421be78e146435f
Mario Osvaldo Nuñez <marionumza@gmail.com>
2025-10-07 12:02:03 -0300

    Update __manifest__.py

--- extra_information/__manifest__.py
+++ extra_information/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Extra_information',
-    'version': '1.0.0',
+    'version': '18.0.0.1',
     'summary': """ Extra_information Summary """,
     'author': '',
     'website': '',
