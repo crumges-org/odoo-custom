2c63bfb0ddc647758d002bf96d6d5fdcbc5ab637
Mario Osvaldo Nuñez <marionumza@gmail.com>
2025-10-07 12:03:01 -0300

    Update __manifest__.py

--- wapp-custom/__manifest__.py
+++ wapp-custom/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Wapp-custom',
-    'version': '1.0.0',
+    'version': '18.0.0.1',
     'summary': """ Wapp-custom Summary """,
     'author': '',
     'website': '',
