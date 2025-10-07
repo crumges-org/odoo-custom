c4ea98cdc2ee341631bc52fe254783287d10bb24
Mario Osvaldo Nuñez <marionumza@gmail.com>
2025-10-07 12:25:54 -0300

    Update __manifest__.py

--- sale_extra_information/__manifest__.py
+++ sale_extra_information/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'sale_extra_information',
-    'version': '18.0.0.1',
+    'version': '18.0.1.0.0',
     'summary': """ Sale Extra Information Summary """,
     'author': '',
     'website': '',
