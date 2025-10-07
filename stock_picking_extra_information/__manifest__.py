fa46ef98a5c56f72d9645c6bec51561d73e6e564
Mario Osvaldo Nuñez <marionumza@gmail.com>
2025-10-07 12:02:44 -0300

    Update __manifest__.py

--- stock_picking_extra_information/__manifest__.py
+++ stock_picking_extra_information/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'stock_picking_extra_information',
-    'version': '1.0.0',
+    'version': '18.0.0.1',
     'summary': """ Stock_picking_extra_information Summary """,
     'author': '',
     'website': '',
