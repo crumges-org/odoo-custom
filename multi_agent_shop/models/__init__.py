6494e73d4c062f4b2d842745e96e04baae2118f5
Mario Nuñez <marionumza@gmail.com>
2025-12-04 11:58:55 -0300

    [DEL] delete multi_agent_shop

--- multi_agent_shop/models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2024 Cybrosys Technologies Pvt. Ltd.
-# Copyright 2024 Crumges
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
-
-from . import res_partner
-from . import sale_order
-from . import account_move
