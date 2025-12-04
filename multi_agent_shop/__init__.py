341d1929bb4c843ba3dc0d770ed681b695f18cd2
Mario Nuñez <marionumza@gmail.com>
2025-12-04 12:22:29 -0300

    [DEL] delete multi_agent_shop

--- multi_agent_shop/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Copyright 2024 Cybrosys Technologies Pvt. Ltd.
-# Copyright 2024 Crumges
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
-
-from . import models
-from . import controllers
--- multi_agent_shop/hooks.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# Hooks desactivados - No crear menú automáticamente
-def post_init_hook(cr, registry):
-    pass
-
-def uninstall_hook(cr, registry):
-    pass
